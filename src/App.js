87286081a68aea8b7e8cd2a8a77f2c9413de27ad
Sudarshan K J <kjsudi@gmail.com>
2020-11-08 19:07:25 +0530

    Uploading initial files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./tree.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="message">
+          This website is currently under development, please come back a little
+          while later
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="copyright">&copy; URWA 2020</p>
       </header>
     </div>
   );
