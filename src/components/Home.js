4848491c8c49ff97fac1991f72f06c35a5cffee1
Sudarshan K J <kjsudi@gmail.com>
2020-11-14 21:45:22 +0530

    Added login icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,24 @@
-import { Image, Heading, Text, Center, VStack } from "@chakra-ui/react";
+import { Box, Image, Heading, Text, Center, VStack } from "@chakra-ui/react";
 import Logo from "assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <Center height="100vh" color="white">
-      <VStack spacing={6}>
-        <Image src={Logo} alt="URWA logo" />
-        <Heading as="h1" size="md" p="4" fontWeight="100" textAlign="center">
-          This website is currently under development. Please come back a little
-          while later
-        </Heading>
-        <Text fontSize="xs"> &copy; URWA 2020</Text>
-      </VStack>
-    </Center>
+    <>
+      <Box pos="absolute" p={4} top={0} right={0} color="white">
+        <Link to="/login">Login</Link>
+      </Box>
+      <Center height="100vh" color="white">
+        <VStack spacing={6}>
+          <Image src={Logo} alt="URWA logo" />
+          <Heading as="h1" size="md" p="4" fontWeight="100" textAlign="center">
+            This website is currently under development. Please come back a
+            little while later
+          </Heading>
+          <Text fontSize="xs"> &copy; URWA 2020</Text>
+        </VStack>
+      </Center>
+    </>
   );
 }
 
