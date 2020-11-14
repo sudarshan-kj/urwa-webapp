d4dc26d4112b2c6f9d479978fff5cc451cda1829
Sudarshan K J <kjsudi@gmail.com>
2020-11-14 21:36:02 +0530

    Styling bug fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ import Logo from "assets/logo.png";
 function Home() {
   return (
     <Center height="100vh" color="white">
-      <VStack>
+      <VStack spacing={6}>
         <Image src={Logo} alt="URWA logo" />
-        <Heading as="h6" size="lg" p="4" fontWeight="100">
+        <Heading as="h1" size="md" p="4" fontWeight="100" textAlign="center">
           This website is currently under development. Please come back a little
           while later
         </Heading>
-        <Text> &copy; URWA</Text>
+        <Text fontSize="xs"> &copy; URWA 2020</Text>
       </VStack>
     </Center>
   );
