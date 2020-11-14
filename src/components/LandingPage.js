354119b48a583e6969adff06f41bfc9a793ba78a
Sudarshan K J <kjsudi@gmail.com>
2020-11-14 20:43:24 +0530

    Fixed the submit button. Enhanced styling. Installed formik and yup

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,67 +6,92 @@ import {
   Text,
   Heading,
   Button,
+  InputRightElement,
+  InputGroup,
 } from "@chakra-ui/react";
-import { FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
+import React from "react";
+import { FormControl, FormLabel } from "@chakra-ui/react";
 import Logo from "assets/logo.png";
 
-const LandingPage = () => (
-  <>
-    <Box display={{ md: "flex" }}>
-      <Center
-        bg="gray.700"
-        w={{ base: "100%", md: "40%" }}
-        h={{ base: "40vh", md: "100vh" }}
-        p={8}
-        color="white"
-      >
-        <Box>
+const LandingPage = () => {
+  const [show, setShow] = React.useState(false);
+  const handleShowClick = () => setShow(!show);
+  return (
+    <>
+      <Box display={{ md: "flex" }}>
+        <Center
+          bg="gray.700"
+          w={{ base: "100%", md: "40%" }}
+          h={{ base: "40vh", md: "100vh" }}
+          p={8}
+          color="white"
+        >
           <Box>
-            <Center flexShrink={0} p={8}>
-              <Image
-                boxSize="100px"
-                objectFit="cover"
-                src={Logo}
-                alt="URWA Logo"
-              />
-            </Center>
-            <Center>
-              <Heading as="p" size="md">
-                Upkar Residency Welfare Association
-              </Heading>
-            </Center>
+            <Box>
+              <Center flexShrink={0} p={8}>
+                <Image
+                  boxSize="100px"
+                  objectFit="cover"
+                  src={Logo}
+                  alt="URWA Logo"
+                />
+              </Center>
+              <Center>
+                <Heading as="p" size="md">
+                  Upkar Residency Welfare Association
+                </Heading>
+              </Center>
+            </Box>
+            <Text fontSize="xs" p={8}>
+              &copy; URWA 2020
+            </Text>
           </Box>
-          <Text fontSize="xs" p={8}>
-            &copy; URWA 2020
-          </Text>
-        </Box>
-      </Center>
-      <Center
-        bg="gray.200"
-        w={{ base: "100%", md: "60%" }}
-        h={{ base: "60vh", md: "100vh" }}
-        p={4}
-        color="black"
-      >
-        <Box w={{ base: "90%", md: "40%" }}>
-          <FormControl id="username" my={8}>
-            <FormLabel>Username</FormLabel>
-            <Input type="text" bg="gray.100" />
-            <FormHelperText textAlign="left">
-              Your registered email id
-            </FormHelperText>
-          </FormControl>
-          <FormControl id="password" my={8}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" bg="gray.100" />
-          </FormControl>
-          <Button bg="gray.600" w={"100%"} color="white">
-            Login
-          </Button>
-        </Box>
-      </Center>
-    </Box>
-  </>
-);
+        </Center>
+        <Center
+          bg="gray.200"
+          w={{ base: "100%", md: "60%" }}
+          h={{ base: "60vh", md: "100vh" }}
+          p={4}
+          color="black"
+        >
+          <Box w={{ base: "90%", md: "40%" }}>
+            <FormControl id="username" my={8}>
+              <FormLabel>User ID</FormLabel>
+              <Input
+                focusBorderColor="teal.400"
+                size="lg"
+                type="text"
+                bg="gray.100"
+              />
+            </FormControl>
+            <FormControl id="password" my={8}>
+              <FormLabel>Password</FormLabel>
+              <InputGroup size="lg">
+                <Input
+                  focusBorderColor="teal.400"
+                  bg="gray.100"
+                  type={show ? "text" : "password"}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button
+                    color="teal.500"
+                    h="1.75rem"
+                    size="xs"
+                    onClick={handleShowClick}
+                  >
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Button type="submit" w={"100%"} colorScheme="teal">
+              Login
+            </Button>
+          </Box>
+        </Center>
+      </Box>
+    </>
+  );
+};
 
 export default LandingPage;
