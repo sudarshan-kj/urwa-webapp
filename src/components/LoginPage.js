749b8f466273afbd5d3041c987e6933c4b848105
Sudarshan K J <kjsudi@gmail.com>
2020-11-17 13:19:57 +0530

    Added form validation

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -12,22 +12,19 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { SlideFade } from "@chakra-ui/transition";
-import { FormControl, FormLabel } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import Logo from "assets/logo.png";
 
 const yupValidationObject = Yup.object({
-  name: Yup.string()
-    .max(30, "Ah! That's too long! Try a bit shorter")
-    .required("Please fill your name"),
-  email: Yup.string()
-    .max(50, "That email is way too long.")
+  userId: Yup.string()
+    .max(50, "User id too long")
     .email("That doesn't look like an email address")
-    .required("Please fill your email"),
-  message: Yup.string()
-    .max(300, "You have exceeded your message limit")
-    .required("Atleast a short message is required"),
+    .required("Enter your user ID"),
+  password: Yup.string()
+    .max(20, "Password is too long")
+    .required("Please enter your password"),
 });
 
 const LoginPage = () => {
@@ -39,7 +36,7 @@ const LoginPage = () => {
 
   const formik = useFormik({
     initialValues: {
-      userid: "",
+      userId: "",
       password: "",
     },
     validationSchema: yupValidationObject,
@@ -87,45 +84,70 @@ const LoginPage = () => {
           color="black"
         >
           <Box transition="transform 0.5s" w={{ base: "90%", md: "40%" }}>
-            <SlideFade in={true} offsetX="60px">
-              <FormControl id="username" my={8}>
-                <FormLabel>User ID</FormLabel>
-                <Input
-                  focusBorderColor="teal.400"
-                  size="lg"
-                  type="text"
-                  bg="gray.100"
-                />
-              </FormControl>
-            </SlideFade>
-            <SlideFade in={true} offsetX="60px">
-              <FormControl id="password" my={8}>
-                <FormLabel>Password</FormLabel>
-                <InputGroup size="lg">
+            <form>
+              <SlideFade in={true} offsetX="60px">
+                <FormControl
+                  id="userId"
+                  my={8}
+                  isInvalid={formik.errors.userId && formik.touched.userId}
+                >
+                  <FormLabel>User ID</FormLabel>
                   <Input
                     focusBorderColor="teal.400"
+                    size="lg"
+                    type="text"
                     bg="gray.100"
-                    type={show ? "text" : "password"}
+                    onChange={(e) => {
+                      if (e.target.value.length <= 30 + 1)
+                        formik.handleChange(e);
+                    }}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.userId}
                   />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      color="teal.500"
-                      h="1.75rem"
-                      size="xs"
-                      onClick={handleShowClick}
-                      _focus={{}}
-                    >
-                      {show ? "Hide" : "Show"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-            </SlideFade>
-            <SlideFade in={true} offsetX="60px">
-              <Button type="submit" w={"100%"} colorScheme="teal" _focus={{}}>
-                Login
-              </Button>
-            </SlideFade>
+                  <FormErrorMessage>{formik.errors.userId}</FormErrorMessage>
+                </FormControl>
+              </SlideFade>
+              <SlideFade in={true} offsetX="60px">
+                <FormControl
+                  id="password"
+                  my={8}
+                  isInvalid={formik.errors.password && formik.touched.password}
+                >
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <InputGroup size="lg">
+                    <Input
+                      id="password"
+                      focusBorderColor="teal.400"
+                      bg="gray.100"
+                      type={show ? "text" : "password"}
+                      onChange={(e) => {
+                        if (e.target.value.length <= 30 + 1)
+                          formik.handleChange(e);
+                      }}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button
+                        color="teal.500"
+                        h="1.75rem"
+                        size="xs"
+                        onClick={handleShowClick}
+                        _focus={{}}
+                      >
+                        {show ? "Hide" : "Show"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                </FormControl>
+              </SlideFade>
+              <SlideFade in={true} offsetX="60px">
+                <Button type="submit" w={"100%"} colorScheme="teal" _focus={{}}>
+                  Login
+                </Button>
+              </SlideFade>
+            </form>
           </Box>
         </Center>
       </Box>
