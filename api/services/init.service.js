3a6a9d73b910d02a928243323549ccf0d85c48cd
Sudarshan K J <kjsudi@gmail.com>
2020-12-01 12:32:50 +0530

    Enhanced config pre check

--- api/services/init.service.js
+++ api/services/init.service.js
@@ -3,24 +3,73 @@ const logger = log4j.getLogger();
 logger.level = "debug";
 const envConfig = require("../config/env.config");
 
-const checkEnvVars = () => {
+const invalidValueSet = (envVariable) => {
+  logger.error(`Invalid value set for ${envVariable}`);
+};
+
+const envVarNotSet = (envVariable) => {
+  logger.error(`${envVariable} env variable is not set`);
+};
+
+const callExit = () => {
+  logger.info("Exiting process");
+  process.exit(1);
+};
+
+const checkUrwaEnv = () => {
   const urwaEnvVar = process.env[envConfig.URWA_ENV_VAR];
   if (!urwaEnvVar) {
-    logger.error(`${envConfig.URWA_ENV_VAR} env variable is not set`);
-    logger.info("Exiting process");
-    process.exit(1);
+    envVarNotSet(envConfig.URWA_ENV_VAR);
+    callExit();
   } else {
     switch (urwaEnvVar) {
       case "local":
       case "production":
         return;
       default: {
-        logger.error(`Invalid value set for ${envConfig.URWA_ENV_VAR}`);
-        logger.info("Exiting process");
-        process.exit(1);
+        invalidValueSet(envConfig.URWA_ENV_VAR);
+        callExit();
       }
     }
   }
+};
+
+const checkTokenEnv = () => {
+  const atEnvVar = process.env[envConfig.AT_ENV_SECRET_VAR];
+  const rtEnvVar = process.env[envConfig.RT_ENV_SECRET_VAR];
+  if (!atEnvVar) {
+    envVarNotSet(envConfig.AT_ENV_SECRET_VAR);
+    callExit();
+  }
+  if (!rtEnvVar) {
+    envVarNotSet(envConfig.RT_ENV_SECRET_VAR);
+    callExit();
+  }
+};
+
+const checkMongoDbEnv = () => {
+  const urwaEnv = process.env[envConfig.URWA_ENV_VAR];
+  if (urwaEnv && urwaEnv !== "local") {
+    logger.info(`Environment: ${urwaEnv}. Performing db config check`);
+    const mongoUserNameEnvVar =
+      process.env[envConfig.MONGO_DB_ENV_USERNAME_VAR];
+    const mongoPasswordEnvVar =
+      process.env[envConfig.MONGO_DB_ENV_PASSWORD_VAR];
+    if (!mongoUserNameEnvVar) {
+      envVarNotSet(envConfig.MONGO_DB_ENV_USERNAME_VAR);
+      callExit();
+    }
+    if (!mongoPasswordEnvVar) {
+      envVarNotSet(envConfig.MONGO_DB_ENV_PASSWORD_VAR);
+      callExit();
+    }
+  }
+};
+
+const checkEnvVars = () => {
+  checkUrwaEnv();
+  checkTokenEnv();
+  checkMongoDbEnv();
 };
 
 const preCheckAppConfig = () => {
