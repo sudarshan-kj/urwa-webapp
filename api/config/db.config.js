0ab0f225c7478bce57e1d08830c8b301fc6c0228
Sudarshan K J <kjsudi@gmail.com>
2020-12-03 19:37:21 +0530

    Update db config

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -2,11 +2,15 @@ module.exports = {
   local: {
     MONGO_DB_USERNAME: "admin",
     MONGO_DB_PASSWORD: "admin",
-    MONGO_DB_URL: "mongodb://localhost/urwa",
+    get MONGO_DB_URL() {
+      return "mongodb://localhost/urwa";
+    },
   },
   production: {
     MONGO_DB_USERNAME: process.env.MONGO_DB_USERNAME,
-    MONGO_DB_PASSWORD: process.env.MONGO_DB_USERNAME,
-    MONGO_DB_URL: `mongodb+srv://kjsudikjsudi:qwerty64504099@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`,
+    MONGO_DB_PASSWORD: process.env.MONGO_DB_PASSWORD,
+    get MONGO_DB_URL() {
+      return `mongodb+srv://${this.MONGO_DB_USERNAME}:${this.MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`;
+    },
   },
 };
