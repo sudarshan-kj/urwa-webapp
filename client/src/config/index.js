4d9a3f6a6d87cce353421c5e73c3116bc47c73c6
Sudarshan K J <kjsudi@gmail.com>
2020-12-03 18:48:43 +0530

    Update config

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,6 @@
-const config = {
-  API_ENDPOINT: "http://192.168.0.199:9000",
-};
+const config =
+  process.env.NODE_ENV === "development"
+    ? { API_ENDPOINT: "http://192.168.0.199:9000" }
+    : { API_ENDPOINT: "https://urwa-backend.vercel.app" };
 
 export default config;
