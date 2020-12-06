796342e956285b57ce25de1dffd183f85f394a6b
Sudarshan K J <kjsudi@gmail.com>
2020-12-06 21:52:33 +0530

    Added permission middleware

--- api/routes/members.js
+++ api/routes/members.js
@@ -1,9 +1,17 @@
 const MembersController = require("../controllers/members.controller");
+const ValidateMiddleware = require("../middlewares/validate.member.middleware");
 const express = require("express");
+const permission = require("../config/permission.config");
 const membersRouter = express.Router();
 
-membersRouter.post("/add", [MembersController.createMember]);
-membersRouter.delete("/:memberId", [MembersController.deleteMember]);
+membersRouter.post("/add", [
+  ValidateMiddleware.hasPermission(permission.CREATE),
+  MembersController.createMember,
+]);
+membersRouter.delete("/:memberId", [
+  ValidateMiddleware.hasPermission(permission.DELETE),
+  MembersController.deleteMember,
+]);
 membersRouter.get("/health", [MembersController.health]);
 
 module.exports = membersRouter;
