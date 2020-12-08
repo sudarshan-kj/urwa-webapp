9a14d5a17a8a5dc90c35883d91115e2a6f6a5c0a
Sudarshan K J <kjsudi@gmail.com>
2020-12-08 08:05:07 +0530

    Update to new database

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -10,7 +10,7 @@ module.exports = {
     MONGO_DB_USERNAME: process.env.MONGO_DB_USERNAME,
     MONGO_DB_PASSWORD: process.env.MONGO_DB_PASSWORD,
     get MONGO_DB_URL() {
-      return `mongodb+srv://${this.MONGO_DB_USERNAME}:${this.MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`;
+      return `mongodb+srv://${this.MONGO_DB_USERNAME}:${this.MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/urwa?retryWrites=true&w=majority`;
     },
   },
 };
