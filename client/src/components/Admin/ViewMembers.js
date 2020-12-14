e44f187eb0ce66af2316cb47e11b4bac2c8da4ea
Sudarshan K J <kjsudi@gmail.com>
2020-12-14 23:43:12 +0530

    Adding chakra table, although its still not working

--- client/src/components/Admin/ViewMembers.js
+++ client/src/components/Admin/ViewMembers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Center } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import {
   Table,
   Thead,
@@ -12,34 +12,35 @@ import {
 
 const ViewMembers = () => {
   return (
-    <Center>List of members </Center>
-    // <Table variant="simple">
-    //   <TableCaption>Imperial to metric conversion factors</TableCaption>
-    //   <Thead>
-    //     <Tr>
-    //       <Th>To convert</Th>
-    //       <Th>into</Th>
-    //       <Th isNumeric>multiply by</Th>
-    //     </Tr>
-    //   </Thead>
-    //   <Tbody>
-    //     <Tr>
-    //       <Td>inches</Td>
-    //       <Td>millimetres (mm)</Td>
-    //       <Td isNumeric>25.4</Td>
-    //     </Tr>
-    //     <Tr>
-    //       <Td>feet</Td>
-    //       <Td>centimetres (cm)</Td>
-    //       <Td isNumeric>30.48</Td>
-    //     </Tr>
-    //     <Tr>
-    //       <Td>yards</Td>
-    //       <Td>metres (m)</Td>
-    //       <Td isNumeric>0.91444</Td>
-    //     </Tr>
-    //   </Tbody>
-    // </Table>
+    <Box>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
 
